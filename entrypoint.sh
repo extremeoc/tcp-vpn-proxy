e5c8425fa745dd1295954f052d48a4b9e69478ba
extremeoc <51958683+extremeoc@users.noreply.github.com>
2022-12-20 11:07:22 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,10 @@ fi
 ENCRYPT="chacha20-ietf-poly1305"
 QR_Path="/qr"
 
-
+#V2Ray Configuration
+V2_Path="/v2"
+mkdir /wwwroot
+mv /v2 /usr/bin/v2
 
 if [ ! -d /etc/shadowsocks-libev ]; then  
   mkdir /etc/shadowsocks-libev
@@ -16,12 +19,14 @@ fi
 sed -e "/^#/d"\
     -e "s/\${PASSWORD}/${Password}/g"\
     -e "s/\${ENCRYPT}/${ENCRYPT}/g"\
+    -e "s|\${V2_Path}|${V2_Path}|g"\
     /conf/shadowsocks-libev_config.json >  /etc/shadowsocks-libev/config.json
 echo /etc/shadowsocks-libev/config.json
 cat /etc/shadowsocks-libev/config.json
 
 sed -e "/^#/d"\
     -e "s/\${PORT}/${PORT}/g"\
+    -e "s|\${V2_Path}|${V2_Path}|g"\
     -e "s|\${QR_Path}|${QR_Path}|g"\
     -e "$s"\
     /conf/nginx_ss.conf > /etc/nginx/conf.d/ss.conf 
@@ -29,8 +34,8 @@ sed -e "/^#/d"\
 if [ "${Domain}" = "no" ]; then
   echo "TCP VPN Proxy"
 else
-  plugin=$(echo -n "host=${Domain};tls" | sed -e 's/\//%2F/g' -e 's/=/%3D/g' -e 's/;/%3B/g')
-  ss="ss://$(echo -n ${ENCRYPT}:${Password} | base64 -w 0)@${Domain}:443" 
+  plugin=$(echo -n "v2ray;path=${V2_Path};host=${Domain};tls" | sed -e 's/\//%2F/g' -e 's/=/%3D/g' -e 's/;/%3B/g')
+  ss="ss://$(echo -n ${ENCRYPT}:${Password} | base64 -w 0)@${Domain}:443?plugin=${plugin}" 
   echo "${ss}" | tr -d '\n' > /wwwroot/index.html
   echo -n "${ss}" | qrencode -s 6 -o /wwwroot/vpn.png
 fi
